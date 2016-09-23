389a6ef6bba281a52f8e6520823e363751129357
lili hualtibamba <lilihperezz@gmail.com>
2016-09-22 22:14:44 -0500

    agregando version2

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 
 ######Versión 0.0.1:
 Se creó un elemento con el mensaje "Añadir una lista", que al dar click muestra un cuadro de texto para que el usuario pueda ingresar el nombre de la lista y un formulario.
+
+######Versión 0.0.2:      
+Se muestra el texto ingresado al dar click en el botón de "Guardar" del formulario (como si fuera título de la lista).            
+Debajo del título se muestra el mensaje clickeable de "Añadir una tarjeta".    
--- js/app.js
+++ js/app.js
@@ -1,11 +1,30 @@
 window.addEventListener("load", function () {
-	var lista= document.getElementById("text");
-	var formLista = document.getElementById("formulario");
-	
-	lista.addEventListener("click", function() {
-		lista.style.display = "none";
-		formLista.style.display = "block";
-	}); 
-});
+    var contLista = document.getElementById("contieneLista");
+    var lista= document.getElementById("text");
+    var formLista = document.getElementById("formulario");
+    var calcular =document.getElementById("calcular");
+    var list = document.getElementById("list");
 
+    lista.addEventListener("click", function() {
+        lista.style.display = "none";
+        formLista.style.display = "block";
+    });
+
+    calcular.addEventListener("click", function(e) {
+        e.preventDefault();
+        lista.style.display = "none";
+        formLista.style.display = "none";
+
+        var tituloLista = document.createElement("div");
+        tituloLista.innerHTML= list.value;
+        contLista.insertBefore(tituloLista,contLista.childNodes[0]);
+       
+        var enlace = document.createElement("a");
+        enlace.href = "#";
+        enlace.style.color = "gray";
+        var nodo = document.createTextNode("Añadir una tarjeta..."); 
+        enlace.appendChild(nodo);
+        contLista.appendChild(enlace);
+    });   
+});
 
